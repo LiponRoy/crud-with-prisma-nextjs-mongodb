0d24e08f96b68561efe896fc3d586ba0c8a4eddb
LiponRoy <liponbdbd@gmail.com>
2023-10-11 16:22:47 +0600

    created post and get api

---
+++ app/api/post/route.ts
@@ -0,0 +1,36 @@
+import prisma from "@/app/libs/prismadb";
+import { NextResponse } from "next/server";
+
+async function main() {
+  try {
+    await prisma.$connect();
+  } catch (error) {
+    return Error("Mongodb connect Unsuccessful");
+  } finally {
+  }
+}
+
+export const GET = async (req: Request, res: NextResponse) => {
+  try {
+    const posts = await prisma.post.findMany();
+    return NextResponse.json({ message: "Success", posts }, { status: 200 });
+  } catch (error) {
+    return NextResponse.json({ message: "Error", error }, { status: 500 });
+  } finally {
+    await prisma.$disconnect();
+  }
+};
+
+export const POST = async (req: Request, res: NextResponse) => {
+  try {
+    const { title, description } = await req.json();
+    await main();
+    const posted = await prisma.post.create({ data: { title, description } });
+    return NextResponse.json({ message: "Data has Posted Successfully", posted }, { status: 200 });
+    
+  } catch (error) {
+    return NextResponse.json({ message: "Error", error }, { status: 500 });
+  } finally {
+    await prisma.$disconnect();
+  }
+};
